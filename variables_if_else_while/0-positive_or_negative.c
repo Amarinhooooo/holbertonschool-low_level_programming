1e7cabe26aab2060292e58c0b81ef323490fd763
Amarinhooooo <10533@holbertonstudents.com>
2025-02-19 13:01:45 +0000

    essai 11

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -5,8 +5,8 @@
 int main(void)
 {
 	int n;
-	
-	srand(time(0));
+
+srand(time(0));
 
 	n = rand() - RAND_MAX / 2;
 
@@ -16,7 +16,7 @@ int main(void)
 	}	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
-	}	else   
+	}	else
 	{
 		printf("%d is positive\n", n);
 	}
