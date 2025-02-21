bc4a87af27b876639d35e0eb189be625395d5f4b
Amarinhooooo <10533@holbertonstudents.com>
2025-02-21 10:00:01 +0000

    31

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -22,11 +22,11 @@ int main(void)
 	}
 	else if (last == 0)
 	{
-		printf("last digit of %d is %d an is 0\n", n, last);
+		printf("last digit of %d is %d and is 0\n", n, last);
 	}
 	else
 	{
-		printf("last digit of %d is %d an is less than 6 and not 0\n", n, last);
+		printf("last digit of %d is %d and is less than 6 and not 0\n", n, last);
 	}
 	return (0);
 }
